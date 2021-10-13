757fcdaef9f53b319574443962c560b1018341bc
mp5maker <khan.photon@gmail.com>
2021-10-13 13:18:34 +0600

    feat: add multilanguage system

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { I18nextProvider } from 'react-i18next'
 import App from './App'
 import './index.scss'
 import MembersContextProvider from './redux/members/context'
+import i18n from './locales'
 
 ReactDOM.render(
   <React.StrictMode>
-    <MembersContextProvider>
-      <App />
-    </MembersContextProvider>
+    <I18nextProvider i18n={i18n}>
+      <MembersContextProvider>
+        <App />
+      </MembersContextProvider>
+    </I18nextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
