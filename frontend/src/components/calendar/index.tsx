91d1397f6cc560f5080bc949b3969c63e6d94b8e
mp5maker <khan.photon@gmail.com>
2021-10-15 18:24:36 +0600

    fix: the calendar now changes language with language change

--- frontend/src/components/calendar/index.tsx
+++ frontend/src/components/calendar/index.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react'
 import ReactCalendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css'
+import useLanguage from '../../hooks/useLanguage'
 import Box from '../box'
 import './calendar.scss'
 
 type TCalendarProps = Partial<React.ComponentProps<typeof ReactCalendar>> & {}
 
-const Calendar: React.FC<TCalendarProps> = ({ ...otherProps }): JSX.Element => {
+const Calendar: React.FC<TCalendarProps> = ({ ...otherProps }: any): JSX.Element => {
+  const { i18n } = useLanguage()
+
   const props = {
+    locale: i18n.languages,
     ...otherProps,
   }
 
