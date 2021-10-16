508e6c9d0ae01424ad5109350c5d073fa5952aac
mp5maker <khan.photon@gmail.com>
2021-10-16 18:21:23 +0600

    feat: add validation for start time and end time for schedule

--- frontend/src/forms/schedule/common.tsx
+++ frontend/src/forms/schedule/common.tsx
@@ -15,6 +15,17 @@ export const schema = ({ t }: { t: (val: string) => string }) =>
     description: yup.string(),
     date: yup.string().required(t('THIS_FIELD_IS_REQUIRED')),
     start_time: yup.string().required(t('THIS_FIELD_IS_REQUIRED')),
-    end_time: yup.string().required(t('THIS_FIELD_IS_REQUIRED')),
+    end_time: yup
+      .string()
+      .test(
+        t('END_TIME_GREATER_THAN_START_TIME'),
+        t('END_TIME_NEEDS_TO_BE_GREATER_THAN_START_TIME'),
+        function (value) {
+          return (
+            Date.parse(`01/01/2011 ${value}:00`) > Date.parse(`01/01/2011 ${this.parent.start_time}:00`)
+          )
+        }
+      )
+      .required(t('THIS_FIELD_IS_REQUIRED')),
     member_id: yup.string().required(t('THIS_FIELD_IS_REQUIRED')),
   })
